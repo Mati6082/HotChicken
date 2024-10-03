ddf2252b0b7da26b71c8213688718e43fc87809f
Mateusz Pakuła <matpakla@gmail.com>
2024-10-03 15:45:13 +0200

    settings activity

--- app/src/main/java/pl/matpakla/shootergame/Settings.kt
+++ app/src/main/java/pl/matpakla/shootergame/Settings.kt
@@ -1,20 +1,20 @@
 package pl.matpakla.shootergame
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import pl.matpakla.shootergame.databinding.ActivityMainBinding
+import pl.matpakla.shootergame.databinding.ActivitySettingsBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class Settings : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -22,12 +22,9 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        binding.play.setOnClickListener {
-            startActivity(Intent(this, Game::class.java))
-        }
-        binding.settings.setOnClickListener {
-            startActivity(Intent(this, Settings::class.java))
-        }
-
+        val difficultyLevels = resources.getStringArray(R.array.difficulty_levels)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, difficultyLevels)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.difficulty.adapter = adapter
     }
 }
